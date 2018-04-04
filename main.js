b86c98e645a79d32fa1d22a3a3407d89c010f92c
Deanna Vickers <deanna2000@gmail.com>
2018-04-04 15:24:32 -0500

    add files

---
+++ main.js
@@ -0,0 +1,12 @@
+const bandNumber = 1
+
+const takeNumber = function (bandName) {
+    bandListItem = bandNumber + ". " + bandName;
+    return bandListItem;
+}
+
+const scum = takeNumber("Galactic Scum")
+console.log(scum)  // This should print "1. Galactic Scum" in the console
+
+const under = takeNumber("Underdogs")
+console.log(under)  // This should print "2. Underdogs" in the console
